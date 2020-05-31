e28956d9e3e3c02c78f6e521569dab8b2061832b
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-05-31 18:28:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+bool allLower(const std::string&);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите слова:" << std::endl;
+	std::string s, word;
+	std::getline(std::cin, s);
+	std::istringstream instr(s);
+	std::istream_iterator<std::string> it(instr), eof;
+	std::size_t count = std::count_if(it, eof, allLower);
+	std::cout << "Количество слов: " << count << std::endl;
+}
+
+bool allLower(const std::string &word)
+{
+	for (auto const &ch : word)
+		if (ch<'a' || ch>'z')
+			return false;
+	return true;
+}
